d0c68d489f8618cd4f1a07c38a94195d73df6238
Xpshnik <turnikmen2011@gmail.com>
2022-04-28 01:50:31 +0300

    Kanban view added + views separated into multiple files

--- __manifest__.py
+++ __manifest__.py
@@ -8,6 +8,9 @@
 	],
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_offer_views.xml',
+        'views/estate_property_type_views.xml',
+        'views/estate_property_tags_views.xml',
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
         'views/res_users.xml',
