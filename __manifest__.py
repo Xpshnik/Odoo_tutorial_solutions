a202da062b5bec1214a1a000d21571f1d4061f2e
Xpshnik <turnikmen2011@gmail.com>
2022-04-27 18:21:00 +0300

    Proprties list added to res_users form. TODO: put views into separate files

--- __manifest__.py
+++ __manifest__.py
@@ -10,6 +10,7 @@
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
+        'views/res_users.xml',
     ],
     'application': True,
     'installable': True,
