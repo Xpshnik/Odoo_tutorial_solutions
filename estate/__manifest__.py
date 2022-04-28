c8a8b2cbcb9695840b35442b63ab19998b6247ff
Xpshnik <turnikmen2011@gmail.com>
2022-04-28 17:17:08 +0300

    Minor views refactoring + fixes

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -13,7 +13,7 @@
         'views/estate_property_tags_views.xml',
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
-        'views/res_users.xml',
+        'views/res_users_views.xml',
     ],
     'application': True,
     'installable': True,
